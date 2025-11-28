7bb8de842393539e8496c889d0b3a76edfeb4c08
Shailendra Veerasamy <shailendra.veerasamy@gmail.com>
2025-11-28 12:03:11 +0100

    My first website

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import requests
+
+st.markdown('''
+# Challenge 1 : Streamlit for restitution
+''')
+
+pickup_longitude = st.number_input("Pickup longitude", format="%.6f")
+pickup_latitude = st.number_input("Pickup latitude", format="%.6f")
+dropoff_longitude = st.number_input("Dropoff longitude", format="%.6f")
+dropoff_latitude = st.number_input("Dropoff latitude", format="%.6f")
+#passenger_count = st.number_input("Passenger count", format="%d")
+
+url = "https://taxifare.lewagon.ai/predict"
+params = {
+    "pickup_datetime": "2014-07-06 19:18:00",
+    "pickup_longitude": pickup_longitude,
+    "pickup_latitude": pickup_latitude,
+    "dropoff_longitude": dropoff_longitude ,
+    "dropoff_latitude": dropoff_latitude,
+    "passenger_count": 2
+}
+
+response = requests.get(url, params=params)
+
+print(response.json())
+
+# Display the result in Streamlit
+st.write("### Predicted Fare:")
+st.json(response.json())
